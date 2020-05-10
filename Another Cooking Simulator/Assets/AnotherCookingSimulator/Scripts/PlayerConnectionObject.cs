beb018720fdd92b5626f381acd7fb5bbda06099e
DAMIAN EZEQUIEL ORZEJOVSKY <44482359@comunidadort.edu.ar>
2020-05-10 00:49:33 -0300

    Base files

---
+++ Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/PlayerConnectionObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerConnectionObject : NetworkBehaviour
+{
+    static bool bandera = true;
+    public GameObject VrUnit;
+    public GameObject PcUnit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cada vez que entra un jugador se crea una unidad fisica en el servidor
+        if(bandera==true)
+        {
+            CmdSpawnMyVR();
+            bandera = false;
+        }
+        else
+        {
+            CmdSpawnMyPC();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    [Command]
+    void CmdSpawnMyVR()
+    {
+        Vector3 position = new Vector3(-1.7f, 1.6f, -2.5f);
+        
+        GameObject go = Instantiate(VrUnit);
+        go.transform.position=position;
+        NetworkServer.Spawn(go);
+    }
+    [Command]
+    void CmdSpawnMyPC()
+    {
+        GameObject go = Instantiate(PcUnit);
+        NetworkServer.Spawn(go);
+    }
+}
---
+++ Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/cuandoSeCocina.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cuandoSeCocina : MonoBehaviour
+{
+    public float tiempoCocinado = 0;
+    //Permito ingresar materiales para el cambio de estado
+    public Material[] material;
+    Renderer rend;
+    bool touchGrill=false;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+    // Se cuenta el tiempo que el pati esta tocando el horno
+    // Y cambia estado dependiendo el tiempo que pasó
+    void Update()
+    {
+        if(touchGrill==true)
+        {
+            tiempoCocinado += Time.deltaTime;
+            cambiarEstado();
+        }
+    }
+    // Cuando un pati toca el horno se cambia a true el bool::touchGrill
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "SurfaceOfGrill")
+        {
+            touchGrill= true;
+        }
+    }
+    // Cuando un pati deja de tocar el horno se cambia a false el bool::touchGrill
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "SurfaceOfGrill")
+        {
+            touchGrill = false;
+        }
+    }
+
+    void cambiarEstado()
+    {
+        if(tiempoCocinado>=12)
+        {
+            rend.sharedMaterial = material[1];
+        }else if(tiempoCocinado>=6)
+        {
+            rend.sharedMaterial = material[0];
+        }
+    }
+    
+}
