71a055a77abd25ef44615912225dc54378fb90de
TobiasElkowich <56679672+TobiasElkowich@users.noreply.github.com>
2020-05-13 11:19:33 -0300

    Union Hamburguesa y Pan
    a casa malos (simon y damian)

---
+++ Another Cooking Simulator/Assets/AnotherCookingSimulator/Prefabs/PruebaColliders/PruebaColliders.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaColliders : MonoBehaviour
+{
+
+    public GameObject hamburguesa;
+    public GameObject anborguesa;
+    private GameObject bun = null;
+    private Renderer rend;
+    private FixedJoint fj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision colliderInfo)
+    {
+        if(this.name == "Collider_Bot" && colliderInfo.collider.name == "BunTOP")
+        {
+            bun = colliderInfo.gameObject;
+            anborguesa.transform.parent = bun.transform;
+            fj = bun.GetComponent<FixedJoint>();
+            fj.connectedBody = anborguesa.GetComponent<Rigidbody>();
+        }
+    }
+}
---
+++ Another Cooking Simulator/Assets/PruebaCollider2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaCollider2 : MonoBehaviour
+{
+
+    public GameObject anborgesa;
+    public Material mat1;
+    public Material mat2;
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision colliderInfo)
+    {
+        Debug.Log(this.name);
+        if(colliderInfo.collider.name == "Plane")
+        {
+            rend = anborgesa.GetComponent<Renderer>();
+            if (this.name == "Collider_Bot")
+            {
+                rend.sharedMaterial = mat1;
+            }
+            if (this.name == "Collider_Top")
+            {
+                rend.sharedMaterial = mat2;
+            }
+        }
+    }
+}
