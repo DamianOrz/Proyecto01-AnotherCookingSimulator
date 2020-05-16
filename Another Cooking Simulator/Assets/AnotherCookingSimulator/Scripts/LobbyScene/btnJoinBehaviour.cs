cd6c92c738c721b1d639943c5a1da3a1a740be18
DAMIAN EZEQUIEL ORZEJOVSKY <44482359@comunidadort.edu.ar>
2020-05-16 00:21:27 -0300

    Cambios en las escenas y las manos de VR

---
+++ Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/LobbyScene/btnJoinBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btnJoinBehaviour : MonoBehaviour
+{
+    public GameObject HostIP;
+    public GameObject HostPORT;
+    public GameObject NetworkManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void sendInfoToNetworkManager()
+    {
+
+    }
+}
