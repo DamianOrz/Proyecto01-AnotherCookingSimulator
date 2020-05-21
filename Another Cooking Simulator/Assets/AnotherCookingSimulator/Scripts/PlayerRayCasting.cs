a7f22532ca820c96ca42d303369200f7ab975271
SimonMirleni <simon.mirleni1@gmail.com>
2020-05-21 12:05:03 -0300

    PcPlayer MouseLook

---
+++ Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/PlayerRayCasting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRayCasting : MonoBehaviour
+{
+    [Range(0.1f,15f)]
+    public float distanceToSee;
+    RaycastHit whatIHit;
+
+    public GameObject Ball;
+    Vector3 position = new Vector3(-6.13f, 1.937f, -5.388f);
+
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
+        if(Physics.Raycast(this.transform.position,this.transform.forward, out whatIHit,distanceToSee))
+        {
+            if(Input.GetKeyDown(KeyCode.E) && whatIHit.collider.gameObject.tag== "Interactable-object")
+            {
+                player.GetComponent<Inventory>().countOfBalls++;
+                Debug.Log("I have a ball");
+                Destroy(whatIHit.collider.gameObject);
+            }
+        }
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && whatIHit.collider.gameObject.tag!= "Interactable-object")
+            {
+                if (player.GetComponent<Inventory>().countOfBalls>0)
+                {
+                    Instantiate(Ball,position,Ball.transform.rotation);
+                    Debug.Log("I put a ball");
+                    player.GetComponent<Inventory>().countOfBalls--;
+                }
+                
+            }
+        }
+    }
+}
