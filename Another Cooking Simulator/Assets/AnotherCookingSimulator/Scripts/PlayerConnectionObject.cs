d275774a7c6ebfd357bac090daf9121fe47e808e
SimonMirleni <simon.mirleni1@gmail.com>
2020-05-22 00:03:31 -0300

    MIRROR

--- Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/PlayerConnectionObject.cs
+++ Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/PlayerConnectionObject.cs
@@ -6,36 +6,35 @@ using Mirror;
 public class PlayerConnectionObject : NetworkBehaviour
 {
     static bool bandera = true;
-    //public GameObject VrUnit;
+    public GameObject VrUnit;
     public GameObject PcUnit;
     // Start is called before the first frame update
     void Start()
     {
-        //FindObjectOfType<AudioManager>().Play("Fry");
         //Cada vez que entra un jugador se crea una unidad fisica en el servidor
-        //if(bandera==true)
-        //{
-        //    CmdSpawnMyVR();
-        //    bandera = false;
-        //}
-        //else
-        //{
-            CmdSpawnMyPC();
-        //}
+        if(bandera==true)
+        {
+            CmdSpawnMyVR();
+            bandera = false;
+        }
+        else
+        {
+          CmdSpawnMyPC();
+          bandera = true;
+        }
     }
     // Update is called once per frame
     void Update()
     {
 
     }
-    /*[Command]
+    [Command]
     void CmdSpawnMyVR()
     {
-        Vector3 position = new Vector3(-1.7f, 1.6f, -2.5f);
         GameObject go = Instantiate(VrUnit);
-        go.transform.position=position;
+
         NetworkServer.Spawn(go);
-    }*/
+    }
     [Command]
     void CmdSpawnMyPC()
     {
