22d75599e2987cee2f27326d509a159ec09fd3c4
SimonMirleni <simon.mirleni1@gmail.com>
2020-05-22 01:33:55 -0300

    ñgsdfjsgp´fospgiusgfdpikjhsfgdsdfgipbhsdfgpjhibsdgfiphsgdfihpbgsdfhipbsgdfhsfgdsdfgpihsdfgbpiosafpioduafdpouhhhhhhhhhhhhhhhhhdb

---
+++ Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/HasAutority.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class HasAutority : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisableOfVR;
+    [SerializeField]
+    Behaviour[] componentsToDisableOfPC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!hasAuthority)
+        {
+            if(this.gameObject.name=="VRPlayer")
+                for (int i = 0; i < componentsToDisableOfVR.Length; i++)
+                {
+                    componentsToDisableOfVR[i].enabled = false;
+                }
+            if(this.gameObject.name=="Player")
+            {
+                for (int i = 0; i < componentsToDisableOfPC.Length; i++)
+                {
+                    componentsToDisableOfVR[i].enabled = false;
+                }       
+            }
+        }
+    }
+    void Update()
+    {
+
+    }
+}
