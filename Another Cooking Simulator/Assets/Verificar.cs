f8fdc42a5272f5c25df63fa0cb343198c713e5bf
SimonMirleni <simon.mirleni1@gmail.com>
2020-06-11 23:28:35 -0300

    Scene

---
+++ Another Cooking Simulator/Assets/Verificar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Verificar : MonoBehaviour
+{
+    public float tiempoCocinado = 0;
+    //Permito ingresar materiales para el cambio de estado
+    public Material[] material;
+    Renderer rend;
+    bool touchGrill = false, estaSonando = false;
+    public ParticleSystem grillSmoke;
+    public GameObject burger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
