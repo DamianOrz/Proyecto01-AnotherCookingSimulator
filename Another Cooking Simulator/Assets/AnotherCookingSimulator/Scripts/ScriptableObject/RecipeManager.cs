495c02110e9330eeed95e94690643600269776e0
SimonMirleni <simon.mirleni1@gmail.com>
2020-06-12 11:50:57 -0300

    ENUMS, SCRIPTABLE OBJECTS

---
+++ Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/ScriptableObject/RecipeManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeManager : MonoBehaviour
+{
+    public enum RECIPE_PARTS
+    {
+        PAN,
+        CARNE,
+        QUESO
+    }
+    public Recipe[] recipes;
+}
