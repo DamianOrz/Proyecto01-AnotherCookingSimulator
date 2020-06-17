d4d603a16e78544f41ef32860e4235b9f743d835
DAMIAN EZEQUIEL ORZEJOVSKY <44482359@comunidadort.edu.ar>
2020-06-16 21:26:52 -0300

    Main Menu

---
+++ Another Cooking Simulator/Assets/CloseGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CloseGame : MonoBehaviour
+{
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
