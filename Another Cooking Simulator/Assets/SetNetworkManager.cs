ea6fdd933442cebf0f6ce93828f445f7762a7b25
DAMIAN EZEQUIEL ORZEJOVSKY <44482359@comunidadort.edu.ar>
2020-06-18 19:06:56 -0300

    MainMenu updated
    Ahora se puede utilizar la conexión a través de tanto steam como localhost. (NOTA: Falta añadir el NetworkDiscovery que permitirá elegir a qué servidor de la red local el usuario se quiere conectar)

---
+++ Another Cooking Simulator/Assets/SetNetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+
+public class SetNetworkManager : MonoBehaviour
+{
+    public TMP_Text newAddress;
+    public NetworkRoomManager myNetworkRoomManager;
+    private string strNewAddress;
+
+    public void GetNewAddress()
+    {
+        if (newAddress is null)
+        {
+            strNewAddress = "localhost";
+        }
+        else
+        {
+            strNewAddress = newAddress.text;
+        }
+        
+        myNetworkRoomManager.SetNetworkAddress(strNewAddress);
+    }
+
+
+    
+        
+    
+}
