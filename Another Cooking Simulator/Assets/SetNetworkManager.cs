b00f92cc037756501e63013fbb36993e092e09a7
DAMIAN EZEQUIEL ORZEJOVSKY <44482359@comunidadort.edu.ar>
2020-06-19 01:49:30 -0300

    Mejoras en el menu principal
    La conexion con Steam no funciona correctamente pero el host local funciona casi a la prefección

--- Another Cooking Simulator/Assets/SetNetworkManager.cs
+++ Another Cooking Simulator/Assets/SetNetworkManager.cs
@@ -8,20 +8,38 @@ public class SetNetworkManager : MonoBehaviour
 {
     public TMP_Text newAddress;
     public NetworkRoomManager myNetworkRoomManager;
+    public SteamManager mySteamManager;
+
+    private Transport localTransport;
+    private Transport steamTransport;
+
+    Transport transportAUtilizar;
     private string strNewAddress;
+
+    private void Start()
+    {
+        localTransport = myNetworkRoomManager.GetComponent<MultiplexTransport>().transports[0];
+        steamTransport = myNetworkRoomManager.GetComponent<MultiplexTransport>().transports[1];
+    }
 
     public void GetNewAddress()
     {
         if (newAddress is null)
         {
             strNewAddress = "localhost";
+            myNetworkRoomManager.GetComponent<MultiplexTransport>().transports[0] = localTransport;
+            myNetworkRoomManager.GetComponent<MultiplexTransport>().transports[1] = steamTransport;
+
         }
         else
         {
             strNewAddress = newAddress.text;
+            myNetworkRoomManager.GetComponent<MultiplexTransport>().transports[1] = localTransport;
+            myNetworkRoomManager.GetComponent<MultiplexTransport>().transports[0] = steamTransport;
         }
-        
-        myNetworkRoomManager.SetNetworkAddress(strNewAddress);
+
+        myNetworkRoomManager.networkAddress = strNewAddress;
+        myNetworkRoomManager.StartClient();
     }
 
 
