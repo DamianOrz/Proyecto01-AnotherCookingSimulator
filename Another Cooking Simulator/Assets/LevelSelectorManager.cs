53db16c4e1bdce8363a67a07bb008a76318350dd
DAMIAN EZEQUIEL ORZEJOVSKY <44482359@comunidadort.edu.ar>
2020-08-26 18:22:33 -0300

    LevelClearedMenu agregado
    No tiene ninguna funcionalidad aún.

---
+++ Another Cooking Simulator/Assets/LevelSelectorManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectorManager : MonoBehaviour
+{
+    public GameObject btnDay0; //Tutorial
+    public GameObject btnDay1;
+    public GameObject btnDay2;
+    public GameObject btnDay3;
+    public GameObject btnDay4;
+    public GameObject btnDay5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+}
