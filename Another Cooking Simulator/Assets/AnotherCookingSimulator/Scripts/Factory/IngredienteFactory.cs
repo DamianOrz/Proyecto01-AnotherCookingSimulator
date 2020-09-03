4cb0d57607d6649c2bdb71d703e34a30c78ffd52
SimonMirleni <simon.mirleni1@gmail.com>
2020-09-03 19:55:17 -0300

    Implementacion de diccionario en factory

--- Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/Factory/IngredienteFactory.cs
+++ Another Cooking Simulator/Assets/AnotherCookingSimulator/Scripts/Factory/IngredienteFactory.cs
@@ -8,16 +8,20 @@ public class IngredienteFactory : MonoBehaviour
     [SerializeField] private Ingrediente _carne;
     [SerializeField] private Ingrediente _cheddar;
 
+    [SerializeField] private Ingrediente[] _ingredientes;
+    private Dictionary<string, Ingrediente> _idToIngrediente;
+
+    private void Awake()
+    {
+        _idToIngrediente = new Dictionary<string, Ingrediente>();
+        foreach (var ingrediente in _ingredientes)
+        {
+            _idToIngrediente.Add(ingrediente.Id, ingrediente);
+        }
+    }
     public Ingrediente Create(string id)
     {
-        switch (id)
-        {
-            case "carne":
-                return Instantiate(_carne);
-            case "cheddar":
-                return Instantiate(_cheddar);
-            default:
-                throw new ArgumentOutOfRangeException($"Ingrediente with id {id} does't exist");
-        }
+        _idToIngrediente.TryGetValue(id, out var ingrediente);
+        return Instantiate(ingrediente);
     }
 }
