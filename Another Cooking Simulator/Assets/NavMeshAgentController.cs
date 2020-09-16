303e717aef2362e6f99e74eb991a73d5b336e40c
DAMIAN EZEQUIEL ORZEJOVSKY <44482359@comunidadort.edu.ar>
2020-09-16 12:26:39 -0300

    [TEST] NavMeshAgent ahora se mueve

---
+++ Another Cooking Simulator/Assets/NavMeshAgentController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentController : MonoBehaviour
+{
+    NavMeshAgent vehicle;
+    Vector3 objective;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vehicle = GetComponent<NavMeshAgent>();
+        objective = GetRandomGameBoardLocation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vehicle.destination = objective;
+       if (vehicle.pathStatus == NavMeshPathStatus.PathComplete && vehicle.remainingDistance == 0)
+       {
+            GenerateNewObjective();
+       }
+    }
+
+    private void GenerateNewObjective()
+    {
+        objective = GetRandomGameBoardLocation();
+    }
+
+    private Vector3 GetRandomGameBoardLocation()
+    {
+        NavMeshTriangulation navMeshData = NavMesh.CalculateTriangulation();
+
+        int maxIndices = navMeshData.indices.Length - 3;
+
+        // pick the first indice of a random triangle in the nav mesh
+        int firstVertexSelected = UnityEngine.Random.Range(0, maxIndices);
+        int secondVertexSelected = UnityEngine.Random.Range(0, maxIndices);
+
+        // spawn on verticies
+        Vector3 point = navMeshData.vertices[navMeshData.indices[firstVertexSelected]];
+
+        Vector3 firstVertexPosition = navMeshData.vertices[navMeshData.indices[firstVertexSelected]];
+        Vector3 secondVertexPosition = navMeshData.vertices[navMeshData.indices[secondVertexSelected]];
+
+        // eliminate points that share a similar X or Z position to stop spawining in square grid line formations
+        if ((int)firstVertexPosition.x == (int)secondVertexPosition.x || (int)firstVertexPosition.z == (int)secondVertexPosition.z)
+        {
+            point = GetRandomGameBoardLocation(); // re-roll a position - I'm not happy with this recursion it could be better
+        }
+        else
+        {
+            // select a random point on it
+            point = Vector3.Lerp(firstVertexPosition, secondVertexPosition, UnityEngine.Random.Range(0.05f, 0.95f));
+        }
+
+        return point;
+    }
+}
