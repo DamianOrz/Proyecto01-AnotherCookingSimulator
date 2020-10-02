ac5db58ec37e4d1efe56ca0e938532fe1852ca7b
DAMIAN EZEQUIEL ORZEJOVSKY <damian.21.10.02@gmail.com>
2020-10-01 23:35:37 -0300

    [FINAL] Ahora los usuarios pueden ver el nivel seleccionado por el host

---
+++ Another Cooking Simulator/Assets/LevelSelectedScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+
+public class LevelSelectedScript : NetworkBehaviour
+{
+    private NetworkRoomManager myNetworkRoomManager;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myNetworkRoomManager = FindObjectOfType<NetworkRoomManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int iLevelSelected = myNetworkRoomManager.GetLevel();
+        if (iLevelSelected == 0)
+        {
+            this.gameObject.GetComponent<TMP_Text>().SetText("Level Selected: TUTORIAL");
+        }
+        else
+        {
+            this.gameObject.GetComponent<TMP_Text>().SetText("Level Selected: " + iLevelSelected.ToString());
+        }
+    }
+}
