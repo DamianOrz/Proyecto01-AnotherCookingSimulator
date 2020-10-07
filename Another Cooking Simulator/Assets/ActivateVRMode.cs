3ebe50fa1c13c81d6c63ce715f3fd0d27d591abb
DAMIAN EZEQUIEL ORZEJOVSKY <damian.21.10.02@gmail.com>
2020-10-07 20:44:01 -0300

    [FINAL] Prototipo menú principal VR

---
+++ Another Cooking Simulator/Assets/ActivateVRMode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateVRMode : MonoBehaviour
+{
+    public GameObject canvas;
+    public Object vrZonePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowVR()
+    {
+        Instantiate(vrZonePrefab);
+    }
+
+    public void DeleteGameobject()
+    {
+        //GameObject a = GameObject.Find("MainMenuCanvas");
+        //canvas.gameObject.SetActive(true);
+        GameObject b = this.transform.root.gameObject;
+        Destroy(b);
+    }
+}
