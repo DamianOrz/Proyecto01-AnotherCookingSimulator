55b616505a68181781893e69f0b6bc7f58975cda
DAMIAN EZEQUIEL ORZEJOVSKY <damian.21.10.02@gmail.com>
2020-11-05 20:26:09 -0300

    [FINAL] Menú sin Scroll

---
+++ Another Cooking Simulator/Assets/ShowHideMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideMenu : MonoBehaviour
+{
+    private bool bMenuShowing;
+    public GameObject LevelSelectorPanel;
+    private Animation levelSelectorAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bMenuShowing = false;
+        levelSelectorAnimator = LevelSelectorPanel.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    public void ShowMenu()
+    {
+        if (bMenuShowing) //Si está activado lo guardamos
+        {
+            levelSelectorAnimator.Play("HideLevelSelector");
+        }
+        else //Lo mostramos
+        {
+            levelSelectorAnimator.Play("ShowLevelSelector");
+        }
+        bMenuShowing = !bMenuShowing;
+    }
+}
